0810a6602f98c08857f578fa3af5c32dff47bcaa
hdjerry <damilolajerryhd@gmail.com>
2021-05-23 16:57:34 +0100

    Updates xxx

--- js/main.js
+++ js/main.js
@@ -18,8 +18,25 @@ const ProfilePage = async () => {
 
     app.innerHTML = `
         <div id="main">
-        <div class="navbar">
-            <div class="nav_left">
+        <div class="app_wrapper app_header">
+            <div class="app_max_width">
+            
+                <button class="flex hamburger js_hamburger_btn">
+                    <svg
+                    height="24"
+                    class="octicon octicon-three-bars"
+                    viewBox="0 0 16 16"
+                    version="1.1"
+                    width="24"
+                    aria-hidden="true"
+                    >
+                    <path
+                        fill-rule="evenodd"
+                        d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"
+                    ></path>
+                    </svg>
+                </button>
+            <div class="flex">
                 <svg 
                 class="octicon octicon-mark-github logo v-align-middle" 
                 height="32" 
@@ -33,13 +50,109 @@ const ProfilePage = async () => {
                 >
                 </path>
                 </svg>
+
+                <div class="flex mbl_links_container">
+                    <div class="header_search_wrapper">
+                        <input type="text" placeholder="Search or jump to..." />
+                        <img
+                        src="https://github.githubassets.com/images/search-key-slash.svg"
+                        alt=""
+                        srcset=""
+                        class="header_search_wrapper_slash-icon"
+                        />
+                    </div>
+                    <div class="app_header_links">
+                        <a href="/#">Pull requests</a><a href="/#">Issues</a
+                        ><a href="/#">Marketplace</a><a href="/#">Explore</a>
+                    </div>
+                    </div>
             </div>
 
-            <div class="nav_right">
-                <div class="nav_pics">
-                   <img src="${user.avatarUrl}" />
+            <div class="flex">
+                <a href="/#" class="header_notification icon"
+                ><svg
+                    class="octicon octicon-bell"
+                    viewBox="0 0 16 16"
+                    version="1.1"
+                    width="16"
+                    height="16"
+                    aria-hidden="true"
+                >
+                    <path
+                    d="M8 16a2 2 0 001.985-1.75c.017-.137-.097-.25-.235-.25h-3.5c-.138 0-.252.113-.235.25A2 2 0 008 16z"
+                    ></path>
+                    <path
+                    fill-rule="evenodd"
+                    d="M8 1.5A3.5 3.5 0 004.5 5v2.947c0 .346-.102.683-.294.97l-1.703 2.556a.018.018 0 00-.003.01l.001.006c0 .002.002.004.004.006a.017.017 0 00.006.004l.007.001h10.964l.007-.001a.016.016 0 00.006-.004.016.016 0 00.004-.006l.001-.007a.017.017 0 00-.003-.01l-1.703-2.554a1.75 1.75 0 01-.294-.97V5A3.5 3.5 0 008 1.5zM3 5a5 5 0 0110 0v2.947c0 .05.015.098.042.139l1.703 2.555A1.518 1.518 0 0113.482 13H2.518a1.518 1.518 0 01-1.263-2.36l1.703-2.554A.25.25 0 003 7.947V5z"
+                    ></path></svg
+                ></a>
+            
+                <div class="flex hide_mbl">
+                    <div tabindex="0" class="github_details">
+                        <details class="icon">
+                        <summary>
+                            <svg
+                            class="octicon octicon-plus"
+                            viewBox="0 0 16 16"
+                            version="1.1"
+                            width="16"
+                            height="16"
+                            aria-hidden="true"
+                            >
+                            <path
+                                fill-rule="evenodd"
+                                d="M8 2a.75.75 0 01.75.75v4.5h4.5a.75.75 0 010 1.5h-4.5v4.5a.75.75 0 01-1.5 0v-4.5h-4.5a.75.75 0 010-1.5h4.5v-4.5A.75.75 0 018 2z"
+                            ></path></svg
+                            ><span class="drop_own_caret"></span>
+                        </summary>
+                        <div class="dropdown">
+                            <a href="/#">New repository</a
+                            ><a href="/#">Import repository</a><a href="/#">New gist</a
+                            ><a href="/#">New organization</a
+                            ><a href="/#">New project</a>
+                        </div>
+                        </details>
+                    </div>
+                    <div class="github_details" tabindex="0">
+                    <details tabindex="-1" class="icon">
+                    <summary>
+                        <img
+                        class="profile_picture"
+                        src="${user.avatarUrl}"
+                        alt=""
+                        srcset=""
+                        /><span class="drop_own_caret"></span>
+                    </summary>
+                    <div class="dropdown">
+                        <div class="modal_section">
+                        <p>Signed in as <strong>${user.name}</strong></p>
+                        </div>
+                        <div class="modal_section">
+                        <a href="/#" class="borederd_link flex"
+                            >🤚<span class="text"
+                            >${user.bio}</span
+                            ></a
+                        >
+                        </div>
+                        <div class="modal_section">
+                        <a href="/#">Your profile</a
+                        ><a href="/#">Your repositories</a
+                        ><a href="/#">Your organizations</a
+                        ><a href="/#">Your projects</a><a href="/#">Your stars</a
+                        ><a href="/#">Your gists</a>
+                        </div>
+                        <div class="modal_section">
+                        <a href="/#">Feature preview</a><a href="/#">Help</a
+                        ><a href="/#">Settings</a><a href="/#">Sign out</a>
+                        </div>
+                    </div>
+                    </details>
+                </div>
                 </div>
             </div>
+
+            </div>
+
         </div>
         <div class="hr"></div>
 
@@ -48,7 +161,13 @@ const ProfilePage = async () => {
                     <div id="pics">
                         <img src="${user.avatarUrl}" />
                         <div class="status">
-                            ${user.status ? user.status.emojiHTML : ''}
+                            ${user.status 
+                                ? 
+                                `<span class="emoji">${user.status.emojiHTML}</span>
+                                <span class="emoji_text">${user.status.message}</span>
+                                ` 
+                                : 
+                                ''}
                         </div>
                     </div>
                     <div class="info">
@@ -62,17 +181,46 @@ const ProfilePage = async () => {
                 </div>
             </div>
 
+
+            
+
         </div>
     `;
 
 
     let repo_container = document.querySelector('#repos');
-    user.repositories.edges.forEach((res) => {
+    user.repositories.nodes.forEach((res) => {
         let div = document.createElement('div');
-        div.setAttribute('class', 'repo');
+        div.setAttribute('class', 'repo flexbox');
         div.innerHTML = `
-            <p>${res.node.name}</p>
-            <p>${res.node.description}</p>
+                <div class="_left">
+                    <p class="flexbox">
+                        <a href="${res.url}">
+                            ${res.name}
+                        </a>
+
+                        ${res.isPrivate ? `<span class="private_tag">Private</span>`  : ''}
+                    </p>
+                    <p>${res.description ? res.description : ''}</p>
+                </div>
+
+                <div class="_right">
+                    <button class="flexbox repo_star_button">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="octicon octicon-star mr-1"
+                        viewBox="0 0 16 16"
+                        version="1.1"
+                        aria-hidden="true"
+                        >
+                        <path
+                            fill-rule="evenodd"
+                            d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
+                        ></path></svg
+                        >
+                        <span>Star</span>
+                    </button>
+               </div>
         `;
 
         repo_container.appendChild(div);
@@ -105,7 +253,8 @@ const getProfile = (user) => {
             user(login: $username) {
                 status {
                     emojiHTML
-                    emoji
+                    message
+                    __typename
                 }
                 name
                 login
@@ -115,21 +264,29 @@ const getProfile = (user) => {
                 followers {
                     totalCount
                 }
-                repositories(first: 20) {
-                    edges {
-                        node {
-                            name
-                            forkCount
-                            stargazerCount
-                            description
-                            languages(first: 1) {
-                                nodes {
-                                    name
-                                    color
-                                }
+                repositories(first: 20, isFork: false) {
+                    nodes {
+                        name
+                        description
+                        url
+                        stargazerCount
+                        updatedAt
+                        forkCount
+                        isPrivate
+                        languages(first: 5, orderBy: {
+                            field: SIZE,
+                            direction: DESC
+                        }) {
+                            nodes {
+                                color
+                                name
+                                __typename
                             }
+                            __typename
                         }
+                        __typename
                     }
+                    __typename
                 }
             }
         }
@@ -195,6 +352,35 @@ function LoginPage () {
 
 
 
+
+function formatDate(date_str) {
+    const d = new Date(date_str);
+    const ye = new Intl.DateTimeFormat("en", {
+        year: "numeric"
+    }).format(d);
+    const mo = new Intl.DateTimeFormat("en", {
+        month: "short"
+    }).format(d);
+    const da = new Intl.DateTimeFormat("en", {
+        day: "2-digit"
+    }).format(d);
+
+    return `${da} ${mo} ${ye}`;
+}
+
+function ActionButton({
+    count,
+    svg
+}) {
+    return count > 0 ?
+        `
+    <button class="action_button">
+    ${svg}
+    ${count}
+  </button>
+  ` :
+        "";
+}
 
 (async function () {
 if (user) {
---
+++ js/technical.js
@@ -0,0 +1,69 @@
+
+
+const gitHubDOM = {
+    getNode: (elementSelector) => document.querySelector(elementSelector),
+    getAllNode: (elementSelector) => document.querySelectorAll(elementSelector),
+};
+
+const gitHubDetailsNode = gitHubDOM.getAllNode(".github_details");
+
+function animateRepoHeader() {
+    const headerUserNode = gitHubDOM.getNode(".repo_app_header-user");
+
+    window.addEventListener("scroll", (e) => {
+        if (window.scrollY >= 370) {
+            headerUserNode.classList.remove("hide");
+        } else {
+            headerUserNode.classList.add("hide");
+        }
+    });
+}
+
+function initMiniModals() {
+    Array.from(gitHubDetailsNode).forEach((currentTarget) => {
+        const detailsNode = currentTarget.querySelector("details");
+
+        currentTarget.addEventListener("blur", () => {
+            setTimeout(() => {
+                detailsNode.open = false;
+                detailsNode.style.pointerEvents = "none";
+            }, 200);
+        });
+
+        currentTarget.addEventListener("focus", () => {
+            detailsNode.open = true;
+            detailsNode.style.pointerEvents = "unset";
+        });
+    });
+}
+
+function initHamBurgerMenu() {
+    const hamBurgerButton = gitHubDOM.getNode(".js_hamburger_btn");
+    const mobileLinks = gitHubDOM.getNode(".mbl_links_container");
+
+    hamBurgerButton.addEventListener("click", () => {
+        const mobileLinksClassList = mobileLinks.classList;
+        const mobileLinksClassListArray = Array.from(mobileLinksClassList);
+
+        if (mobileLinksClassListArray.includes("is_open")) {
+            mobileLinksClassList.remove("is_open");
+        } else {
+            mobileLinksClassList.add("is_open");
+        }
+    });
+}
+
+const fetchAndMapReposAction = async () => {
+    
+    const repoContainerNode = gitHubDOM.getNode(".repo_container");
+    const pageLoaderNode = gitHubDOM.getNode(".app_page_loader");
+
+    pageLoaderNode.classList.add("hide");
+    // repoContainerNode.classList.remove("hide");
+
+    // animateRepoHeader();
+};
+
+fetchAndMapReposAction();
+initMiniModals();
+initHamBurgerMenu();
